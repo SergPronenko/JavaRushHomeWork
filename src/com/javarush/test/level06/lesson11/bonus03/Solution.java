313f4a86a635cc3889aa4a92c227683c76d2c637
Admin <pronenko_sn@i.ua>
2016-07-14 17:47:36 +0300

    hi

---
+++ src/com/javarush/test/level06/lesson11/bonus03/Solution.java
@@ -0,0 +1,52 @@
+package com.javarush.test.level06.lesson11.bonus03;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* Задача по алгоритмам
+Задача: Написать программу, которая вводит с клавиатуры 5 чисел и выводит их в возрастающем порядке.
+Пример ввода:
+3
+2
+15
+6
+17
+Пример вывода:
+2
+3
+6
+15
+17
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        BufferedReader reader  = new BufferedReader(new InputStreamReader(System.in));
+
+        /*int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+        int d = Integer.parseInt(reader.readLine());
+        int e = Integer.parseInt(reader.readLine());*/
+        int array[] = new int[5];
+        for (int i = 0; i<=4; i++){
+            array[i] = Integer.parseInt(reader.readLine());
+        }
+        int temp =0;
+        for (int i=array.length-1; i>=0; i--){
+            for (int j=0; j<i; j++){
+                if(array[j]>array[j+1]){
+                    temp = array[j];
+                    array[j] =array[j+1];
+                    array[j+1] = temp;
+
+                }
+            }
+        }
+        for (int i=0; i<=array.length-1; i++){
+            System.out.println(array[i]);
+        }
+    }
+}
